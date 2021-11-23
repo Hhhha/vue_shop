fc7b9a4a3f4357dbbc993701cca6a335061e5d9a
luke <luke@goodstar.vip>
2021-11-23 16:36:49 +0800

    init

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+//  导入全局样式
+import './assets/css/global.css'
+import axios from 'axios'
+axios.defaults.baseURL = 'http://www.baidu.com/'
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
